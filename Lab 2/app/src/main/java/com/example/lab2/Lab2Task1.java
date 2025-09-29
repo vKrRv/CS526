092a150883f7aafb00ae5befed39bbad1dc108c8
Ali Albaqqal <alawi-522@hotmail.com>
2025-09-29 10:32:28 +0300

    feat: add a navigation button to the next task

--- Lab 2/app/src/main/java/com/example/lab2/Lab2Task1.java
+++ Lab 2/app/src/main/java/com/example/lab2/Lab2Task1.java
@@ -2,8 +2,10 @@ package com.example.lab2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +17,22 @@ public class Lab2Task1 extends AppCompatActivity {
 
     private int count = 0;
     private TextView btnCount;
+    private Button nextBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lab2_task1);
+
+        nextBtn = findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Lab2Task1.this, Lab2Task2.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
